bbcafb09d17371e1920dd57ee531768a9cd90033
hicham.arahhou <hichamarahhou@yahoo.fr>
2019-09-27 13:23:02 +0200

    'Student' validators tests fixed : now operationnal.

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
   test "should have the necessary required validators" do
-    post = Student.new
+    student = Student.new
     assert_not student.valid?
-    assert_equal [:name, :first_name], post.errors.keys
+    assert_equal [:name, :first_name], student.errors.keys
 
     student.name = "Name"
     student.first_name = "FirstName"
